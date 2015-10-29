f4be554199e63969ee83dd2324a35980ce5a598d
Jose Mendez <josecarlos@windowslive.com>
2015-10-28 20:17:23 -0600

    Nuget recognizable for Glimpse updater tool.

--- Glimpse.ApplicationInsights/Properties/AssemblyInfo.cs
+++ Glimpse.ApplicationInsights/Properties/AssemblyInfo.cs
@@ -3,40 +3,23 @@
 //     Copyright (c) Glimpse. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Glimpse.Core.Extensibility;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Glimpse.ApplicationInsights")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Glimpse.ApplicationInsights")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f75f1dd4-1935-42ea-a4a0-9dc92f727075")]
+[assembly: AssemblyTitle("Glimpse for ApplicationInsights")]
+[assembly: AssemblyDescription("Main extensibility implementations for running Glimpse with ApplicationInsights.")]
+[assembly: AssemblyProduct("Glimpse.ApplicationInsights")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// Version is in major.minor.build format to support http://semver.org/
+// Keep these three attributes in sync
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")] // Used to specify the NuGet version number at build time
+
+[assembly: CLSCompliant(true)]
+[assembly: NuGetPackage("Glimpse.ApplicationInsights")]
